6d70fd6929a930c82627b924c451e56bacba222c
yuri cardoso <yurycards@gmail.com>
2025-03-28 21:44:59 -0300

    atividae feita

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("o que é o que é...");
+Thread.Sleep(3000);
+Console.WriteLine("o que o peixe faz?...");
+Thread.Sleep(3000);
+Console.WriteLine("nada");
+Thread.Sleep(3000);
